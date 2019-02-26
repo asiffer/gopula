2bfa84b01edd89c9799a710b99649dac20ed0505
Alban Siffer <alban.siffer@irisa.fr>
2019-02-26 17:30:32 +0100

    new csv file tests

--- csv_test.go
+++ csv_test.go
@@ -6,31 +6,31 @@ import (
 	"gonum.org/v1/gonum/mat"
 )
 
-func TestLoadCsv(t *testing.T) {
-	M, err := LoadCSV(csv, ',', false)
-	n, p := M.Dims()
-	if err != nil {
-		t.Error(err.Error())
-	} else if n != 2000 || p != 3 {
-		t.Errorf("Bad matrix dimensions (expected (2000,3), got (%d, %d))", n, p)
-	}
-}
+var (
+	testCSV = "resources/data.csv"
+)
+
 
 func TestSaveCsv(t *testing.T) {
 	n := 4
-	p := 4
+	p := 3
 	M := mat.NewDense(n, p, nil)
 	M.Set(0, 1, 3.5)
-	SaveCSV(M, "/tmp/test.csv", ',')
-	N, err := LoadCSV("/tmp/test.csv", ',', false)
+	err := SaveCSV(M, testCSV, ',')
 	if err != nil {
-		t.Errorf("Error in loading previously saved matrix (%s)", err.Error())
+		t.Errorf("Error in saving matrix (%s)", err.Error())
 	}
-	for i := 0; i < n; i++ {
-		for j := 0; j < p; j++ {
-			if N.At(i, j) != M.At(i, j) {
-				t.Errorf("Bad matrix element [%d, %d]: expected %f, got %f", i, j, M.At(i, j), N.At(i, j))
-			}
-		}
+}
+	
+func TestLoadCsv(t *testing.T) {
+	M, err := LoadCSV(testCSV, ',', false)
+	n, p := M.Dims()
+	if err != nil {
+		t.Error(err.Error())
+	} else if n != 4 || p != 3 {
+		t.Errorf("Bad matrix dimensions (expected (4,3), got (%d, %d))", n, p)
 	}
+}
+
+}
 }
